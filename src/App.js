138ea424823238f59f124ff9652045f444011291
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-08-29 12:45:59 +0530

    ﻿not much chnaged

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // return React.createElement("h1", {}, "Hello World");  We can also write like this
+  // <h4>Hello</h4>;
+
+  const Person = () => <h2>Hello World</h2>;
+  // If we write inside {} than we have to return it
+  const Message = () => {
+    return <p>How are You???</p>;
+  };
+
+  // for small quantity we can define each book separately but if we thousands of book that what??? so we have to use map method and for that we have to make an array of object
+  // const firstBook = {
+  //   img: "https://images-na.ssl-images-amazon.com/images/I/41B2C2Frv5L._SX322_BO1,204,203,200_.jpg",
+  //   title: "lets go to chilhood",
+  //   author: "asha chitroda",
+  // };
+
+  // const secondBook = {
+  //   img: "https://images-eu.ssl-images-amazon.com/images/I/41LD2VhDn8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+  //   title: "War Book",
+  //   author: "ravi chitroda",
+  // };
+
+  const books = [
+    {
+      img: "https://images-na.ssl-images-amazon.com/images/I/51I6vQLJD+L._SX258_BO1,204,203,200_.jpg",
+      title: "Spirituality For Leadership & Success",
+      author: "Vivekananda",
+    },
+    {
+      img: "https://images-eu.ssl-images-amazon.com/images/I/51YP7fM361S._SX198_BO1,204,203,200_QL40_FMwebp_.jpg",
+      title: "The Time Machine",
+      author: "H. G. Wells ",
+    },
+    {
+      img: "https://images-eu.ssl-images-amazon.com/images/I/51PJyvcfPGL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      title: "Life's Amazing Secrets",
+      author: "Gaur Gopal Das",
+    },
+  ];
+
+  const Book = (props) => {
+    //here we can write either props or can direct pass key or properties into this like {img, title, author} this is called destructuring
+    // const Book = ({ img, title, author, children }) => {
+    // console.log("props", props);
+    const { img, title, author } = props.book;
+    // console.log("first Props", props);
+    // const title = () => <h2>Wuthering Heights</h2>;
+    // const title = "Love you like you do";
+    // const author = "ravi chitroda";
+    // const img =
+    //   "https://m.media-amazon.com/images/I/81j8i9slKIL._AC_UY327_FMwebp_QL65_.jpg";
+
+    // return <article>This is Book</article>;
+
+    return (
+      <div>
+        {/* <img
+          src="https://m.media-amazon.com/images/I/81j8i9slKIL._AC_UY327_FMwebp_QL65_.jpg"
+          alt=""
+        /> */}
+
+        {/* <img src={props.img} alt="book" />
+        <h1>{props.title}</h1>
+        <h3>{props.author.toLocaleUpperCase()}</h3> */}
+
+        {/* if we destructuring props as on line 27 of const Book than we don't need to write props.img we just write img as props has now destructured */}
+        <img src={img} alt="book" />
+        <h1>{title}</h1>
+        <h3>
+          <span>by : </span>
+          {author.toLocaleUpperCase()}
+        </h3>
+        {/* {children}   for children of props we have to pass children compulsory */}
+        {props.children}
+        {/* {props.type}
+        {props.price} */}
+        {/* {console.log("second Props", props)} */}
+      </div>
+    );
+  };
+
+  // const Image = () => {
+  //   return (
+  //     <img
+  //       src="https://m.media-amazon.com/images/I/81j8i9slKIL._AC_UY327_FMwebp_QL65_.jpg"
+  //       alt=""
+  //     />
+  //   );
+  // };
+
+  // const title = () => <h2>Wuthering Heights</h2>;   //We can also write here and also can be write inside the function
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Person />
+      <h2>Hello There</h2>
+      <ul>
+        <li>
+          <a href="#">Hello Everyone</a>
+        </li>
+        <Message />
+      </ul>
     </div>
   );
 }
